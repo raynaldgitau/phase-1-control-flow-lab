65c8e974a31bb1508ba489fd3f2cb8e3d213111e
Raynald <raynald.gitau@student.moringaschool.com>
2023-03-19 18:08:58 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+if(distance<= 400) {
+  return 'This one is on me!';
+}
+ if (distance > 2000 && distance <=2500) {
+  return 'I will gladly take your thirty bucks.'; 
+}else {
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+console.log(scuberGreetingForFeet(199));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+    return city == 'NYC' ? 'Ok, sounds good.'
+              :'No go.';
+}
+console.log(ternaryCheckCity(Pittsburgh))
+
+function switchOnCharmFromTip(status){
+    console.log(status);
+  let message;
+      switch (status) {
+      case ('generous'):
+           message='Thank you so much.';
+           break;
+      case ('not as generous'):
+            message='Thank you.'
+            break;
+            
+      default:
+        message = 'Bye.';
+        console.log('bye.');    
+      }
+      return message
 }
